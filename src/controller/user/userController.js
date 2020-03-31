2f43d12118bb3737d1f014af9ecd583103df6bd8
johnnyloreano <laureano.pedrojoao@gmail.com>
2020-03-31 20:28:27 -0300

    Implementado login BACKEND - Necessario criação de teste

--- src/controller/user/userController.js
+++ src/controller/user/userController.js
@@ -3,18 +3,43 @@ import admin from '../../database/connection';
 const db = admin.firestore();
 module.exports = {
 
-    async get(request,response){
+    async get(request, response) {
         let res = new Map;
         await db.collection('user').get().then((snapshot) => {
-                    snapshot.forEach((doc) => {
-                        res[doc.id] = doc.data();
-                    });
-            })
-        .catch((err) => {
-          console.log('Error getting documents', err);
-        });  
+            snapshot.forEach((doc) => {
+                res[doc.id] = doc.data();
+            });
+        })
+            .catch((err) => {
+                console.log('Error getting documents', err);
+            });
 
         return response.json(res);
+    },
+
+    async post(request, response) {
+        let data = null
+
+        try {
+            let { user, password } = request.body;
+            if(user === null || password === null){
+                return response.status(404).json({"error": "Não foram enviados os dados.Favor, preencher com dados."});
+            }
+
+            let datas = Array();
+            await db.collection('user').where("usuario","==",user).where("password", "==", password).get().then((snapshot) => {
+                snapshot.forEach(
+                    (res) => {data = res.data()}
+                )
+            });
+            if(data === null){
+                return response.status(401).json({"error": "Usuário inexistente/Senha inválida!"});
+            }
+            return response.status(200).json(data);
+
+        }
+        catch{
+            return response.status(500).json(data);
+        }
     }
-    
 }
--- src/routes.js
+++ src/routes.js
@@ -6,7 +6,9 @@ const userController = require('./controller/user/userController');
 
 
 routes.get('/', (req, res) => res.json({ message: 'Hello world' }));
+
 routes.get('/users', userController.get);
+routes.post('/users', userController.post);
 
 
 export default routes;
