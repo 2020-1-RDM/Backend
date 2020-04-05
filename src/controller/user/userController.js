24e01ddb72ba80b7c09e07aa84533bcae5bf4b38
johnnyloreano <laureano.pedrojoao@gmail.com>
2020-04-05 01:43:13 -0300

    Created User CRUD

--- src/controller/user/userController.js
+++ src/controller/user/userController.js
@@ -6,17 +6,129 @@ const jwt = require("jsonwebtoken");
 module.exports = {
 
     async get(request, response) {
-        let res = new Map;
-        await db.collection('user').get().then((snapshot) => {
-            snapshot.forEach((doc) => {
-                res[doc.id] = doc.data();
-            });
-        })
-            .catch((err) => {
-                console.log('Error getting documents', err);
-            });
+        try{
+            let {user} = request.body;
+            if(!user){
+             
+                return response.status(404).json({ "error": "Não foram enviados os dados." });
 
-        return response.json(res);
+            }
+        let userCollection = db.collection('user');
+        let result = null
+        await userCollection.where("usuario", "==", user).get().then(
+            (snapshot) => {
+                return snapshot.forEach(
+                    (res) => { result = res.data() }
+                )
+            });
+            if(!result){
+                return response.status(404).json({ "error": "Não foi encontrado." });
+            }
+        return response.status(200).json(res);
+
+        }
+        catch(e){
+            return response.status(500).json({ "error": `Erro durante o processamento do login. Espere um momento e tente novamente! Erro : ${e}` });
+                
+        }
+    },
+    async insert(request, response) {
+        try{
+            let {user, cpf, mentorFlag, email, password, name, linkedin, phone} = request.body;
+
+        let userCollection = db.collection('user');
+
+        let dbVerification = null;
+        await userCollection.where("user", "==", user).get().then(
+            (snapshot) => {
+                return snapshot.forEach(
+                    (res) => { dbVerification = res.data() }
+                )
+            });
+            if(dbVerification){
+                return response.status(400).send({"error" : "Usuário já existe."});
+            }
+
+        await userCollection.add({
+                                    'user': user,
+                                    'password': password,
+                                    'name': name,
+                                    'cpf': cpf,
+                                    'phone' : phone,
+                                    'linkedin': linkedin,
+                                    'email' : email,
+                                    'mentorFlag' : mentorFlag
+                                })
+            return response.status(201).send();
+
+        }
+        catch(e){
+            return response.status(500).json({ "error": `Erro durante o processamento do login. Espere um momento e tente novamente! Erro : ${e}` });
+                
+        }
+    },
+
+    async update(request, response) {
+        try{
+            let {user, cpf, mentorFlag, email, password, name, linkedin, phone} = request.body;
+
+        let userCollection = db.collection('user');
+
+        let dbVerification = null;
+        await userCollection.where("user", "==", user).get().then(
+            (snapshot) => {
+                return snapshot.forEach(
+                    (res) => { dbVerification = res.id }
+                )
+            });
+            if(!dbVerification){
+                return response.status(400).send({"error" : "Usuário não existe."});
+            }
+
+        await userCollection.doc(dbVerification).set({
+                                    'user': user,
+                                    'password': password,
+                                    'name': name,
+                                    'cpf': cpf,
+                                    'phone' : phone,
+                                    'linkedin': linkedin,
+                                    'email' : email,
+                                    'mentorFlag' : mentorFlag
+                                })
+            return response.status(200).send();
+
+        }
+        catch(e){
+            return response.status(500).json({ "error": `Erro durante o processamento do login. Espere um momento e tente novamente! Erro : ${e}` });
+                
+        }
+    },
+
+    async delete(request, response) {
+        try{
+            let {user} = request.body;
+
+        let userCollection = db.collection('user');
+
+        let dbVerification = null;
+        await userCollection.where("user", "==", user).get().then(
+            (snapshot) => {
+                return snapshot.forEach(
+                    (res) => { dbVerification = res.id }
+                )
+            });
+            if(!dbVerification){
+                return response.status(400).send({"error" : "Usuário não existe."});
+            }
+
+        await userCollection.doc(dbVerification).delete();
+            return response.status(200).send();
+
+        }
+        catch(e){
+            return response.status(500).json({ "error": `Erro durante o processamento do login. Espere um momento e tente novamente! Erro : ${e}` });
+                
+        }
     },
 
 
@@ -25,36 +137,36 @@ module.exports = {
 
             let { user, password } = request.body;
 
-            if(!user || !password){
+            if (!user || !password) {
 
-                return response.status(404).json({"error": "Não foram enviados os dados."});
-                
+                return response.status(404).json({ "error": "Não foram enviados os dados." });
+
             }
             let userCollection = db.collection('user')
             let result = null
-            await userCollection.where("usuario","==",user).where("password", "==", password).get().then(
+            await userCollection.where("usuario", "==", user).where("password", "==", password).get().then(
                 (snapshot) => {
                     return snapshot.forEach(
-                        (res) => {  result = res.data()}
+                        (res) => { result = res.data() }
                     )
-            });
+                });
 
-            if(!result){
-                return response.status(401).json({"error": "Erro de autenticação!"});
+            if (!result) {
+                return response.status(401).json({ "error": "Erro de autenticação!" });
             }
             let token = jwt.sign({
-                'cpf' : result['cpf'],
-                'email' : result['email']
+                'cpf': result['cpf'],
+                'email': result['email']
 
             }, process.env.JWT_KEY, {
-                expiresIn : '1h'
+                expiresIn: '1h'
             });
 
-            return response.status(200).json({'token' : token});
+            return response.status(200).json({ 'token': token });
 
         }
-        catch(e){
-            return response.status(500).json({"error": `Erro durante o processamento do login. Espere um momento e tente novamente! Erro : ${e}`});
+        catch (e) {
+            return response.status(500).json({ "error": `Erro durante o processamento do login. Espere um momento e tente novamente! Erro : ${e}` });
         }
     }
 }
--- src/routes.js
+++ src/routes.js
@@ -8,7 +8,11 @@ const userController = require('./controller/user/userController');
 routes.get('/', (req, res) => res.json({ message: 'Hello world' }));
 
 routes.get('/users', userController.get);
-routes.post('/users', userController.login);
+routes.post('/users', userController.insert);
+routes.put('/users', userController.update);
+routes.delete('/users', userController.delete);
+
+routes.post('/login', userController.login);
 
 
 export default routes;
